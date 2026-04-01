df64df404ba5b6dc1b869593594188b9b0240c3a
Bishwajit Pattanaik <bishwajit.pattanaik2004@gmail.com>
2026-04-01 12:11:24 +0530

    Task 3: Added setter methods to Employee class

--- employee-service/src/main/java/com/hpe/employeeservice/Employee.java
+++ employee-service/src/main/java/com/hpe/employeeservice/Employee.java
@@ -2,11 +2,11 @@ package com.hpe.employeeservice;
 
 public class Employee {
 
-    private final String employee_id;
-    private final String first_name;
-    private final String last_name;
-    private final String email;
-    private final String title;
+    private String employee_id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String title;
 
     public Employee(String employee_id,
                     String first_name,
@@ -32,9 +32,17 @@ public class Employee {
                 + title;
     }
 
+    // Getters
     public String getEmployee_id() { return employee_id; }
     public String getFirst_name()  { return first_name; }
     public String getLast_name()   { return last_name; }
     public String getEmail()       { return email; }
     public String getTitle()       { return title; }
+
+    // Setters
+    public void setEmployee_id(String employee_id) { this.employee_id = employee_id; }
+    public void setFirst_name(String first_name)   { this.first_name = first_name; }
+    public void setLast_name(String last_name)     { this.last_name = last_name; }
+    public void setEmail(String email)             { this.email = email; }
+    public void setTitle(String title)             { this.title = title; }
 }
