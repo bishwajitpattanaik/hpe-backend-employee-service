4d5588c08262dde8d817e8f5d340d0decb25f89f
Bishwajit Pattanaik <bishwajit.pattanaik2004@gmail.com>
2026-04-01 12:12:22 +0530

    Task 3: Added POST /employees endpoint to add new employee

--- employee-service/src/main/java/com/hpe/employeeservice/EmployeeController.java
+++ employee-service/src/main/java/com/hpe/employeeservice/EmployeeController.java
@@ -2,6 +2,8 @@ package com.hpe.employeeservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,13 @@ public class EmployeeController {
     @GetMapping(path = "/", produces = "application/json")
     public Employees getEmployees()
     {
+        return employeeManager.getAllEmployees();
+    }
+
+    @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+    public Employees addEmployee(@RequestBody Employee employee)
+    {
+        employeeManager.addEmployee(employee);
         return employeeManager.getAllEmployees();
     }
 }
